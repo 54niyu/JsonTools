4e9724f9e853fd222b47525332506815f938d5a4
54niyu@sina.com <54niyu.cn>
2016-01-25 18:18:19 +0800

    Support UTF-16

--- JsonParser/encode.cpp
+++ JsonParser/encode.cpp
@@ -1,9 +1,8 @@
 #include<stdlib.h>
 #include<stdio.h>
-#include<wchar.h>
 #include<assert.h>
 
-    /*
+/*
     Char.number range | UTF - 8 octet sequence
     (hexadecimal) | (binary)
     -------------------- + -------------------------------------------- -
@@ -11,7 +10,7 @@
     0000 0080 - 0000 07FF | 110xxxxx 10xxxxxx
     0000 0800 - 0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
     0001 0000 - 0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
-    */
+*/
 int utf8ToCp(unsigned* dist, unsigned char* src, size_t size){
     //convert utf8 to Unicode code point
     unsigned* p = dist;
@@ -74,7 +73,7 @@ int cpToUtf8(unsigned char* dist, unsigned* src, size_t size){
 	    ptr++;
 	}
 	else{
-	    return;
+	    return -1;
 	}
 	*dist = '\0';
 	return (dist - p);
@@ -104,9 +103,7 @@ Graphically, steps 2 through 4 look like :
 U' = yyyyyyyyyyxxxxxxxxxx
 W1 = 110110yyyyyyyyyy
 W2 = 110111xxxxxxxxxx
-*/
 
-/*
 Decoding of a single character from UTF-16 to an ISO 10646 character
 value proceeds as follows. Let W1 be the next 16-bit integer in the
 sequence of integers representing the text. Let W2 be the (eventual)
@@ -131,16 +128,32 @@ W2 as its 10 low-order bits.
 
 */
 int utf16leToCp(unsigned* dist,unsigned char* src, size_t size){
-
+    //convert UTF-16 Little Endian to CodePoint
     unsigned *p = dist;
     unsigned char* ptr = src;
-    while (ptr < ptr + size){
+    while (ptr < src + size){
+	    *dist = *ptr++;
+	    unsigned short W1 = *ptr++;
+	    W1 = W1 << 8 + *dist;
+	    if (W1 >=0xd800 && W1<=0xdfff){
+		*dist = *ptr++;
+		unsigned short W2 = *ptr++;
+		W2 = W2 << 8 + *dist;
 
+		*dist = W1 & 0x3ff;
+		*dist = *dist << 10;
+		*dist |= W2 & 0x3ff;
+		dist++;
+	    }
+	    else{
+		*dist = W1;
+		dist++;
+	    }
     }
     return (dist-p);
 }
 int cpToUtf16le(unsigned char* dist, unsigned* src, size_t size){
-    //convert code point to UTF-16 Little Endian
+    //convert Unicode Code Point to UTF-16 Little Endian
     unsigned char* p=dist;
     unsigned* ptr = src;
     while (ptr < src + size){
@@ -167,17 +180,30 @@ int cpToUtf16le(unsigned char* dist, unsigned* src, size_t size){
     return (dist - p);
 }
 int utf16beToCp(unsigned* dist,unsigned char* src, size_t size){
-    
+   //convert UTF-16 Big Endian to Unicode Code Point 
     unsigned *p = dist;
     unsigned char* ptr = src;
-    while (ptr < ptr + size){
+    while (ptr < src + size){
+	*dist = *ptr++;
+	unsigned short W1 = *dist << 8 | *ptr++;
+	if (W1 >= 0xd800 &&W1 <= 0xdfff){
+	    *dist = *ptr++;
+	    unsigned short W2 = *dist<<8|*ptr++;
 
-
+	    *dist = W1 & 0x3ff;
+	    *dist = *dist << 10;
+	    *dist |= W2 & 0x3ff;
+	    dist++;
+	}
+	else{
+	    *dist = W1;
+	    dist++;
+	}
     }
     return (dist-p);
 }
 int cpToUtf16be(unsigned char* dist, unsigned* src, size_t size){
-    //convert CodePoint to UTF_16 Big Endian
+    //convert Unicode Code Point to UTF_16 Big Endian
     unsigned char*p = dist;
     unsigned* ptr = src;
     while (ptr < src + size){
@@ -219,6 +245,9 @@ int main(){
     unsigned op[1024];
 
     FILE* fp = fopen(input, "rb");
+    unsigned char a = 0x1;
+    a=a << 7;
+    printf("%x", a);
 
     int size = 0;
     while (!feof(fp)){
@@ -226,17 +255,18 @@ int main(){
     }
     buffer[size] = '\0';
 
-    int ss=utf8ToCp(op, buffer, size);
+//    int ss=utf8ToCp(op, buffer, size);
+    int ss = utf16beToCp(op, buffer+2, size-2);
  //   cpToUtf8(buffer2, op, ss);
   //  cpToUtf16le(buffer2, op, ss);
-    cpToUtf16be(buffer2, op, ss);
+    cpToUtf16le(buffer2, op, ss);
 
  //   for (int i = 0; i < size; i++){
 	//printf("%x-%x ", buffer[i], buffer2[i]);
  //   }
     FILE* ffp = fopen(output, "wb");
-
-    fputc(0xfe, ffp);    fputc(0xff, ffp);
+ fputc(0xff, ffp);
+    fputc(0xfe, ffp);   
     fwrite(buffer2, 1, 1024, ffp);
     printf("here");
     return 0;
